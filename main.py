7f9921ea7ca01a56557b4145daede7f59258f02e
Mitch <mitch.mulder88@gmail.com>
2019-04-05 21:08:45 -0400

    Added more comments

--- main.py
+++ main.py
@@ -46,16 +46,17 @@ def generatePlots(name, base_network, x_train, x_test, y_train, y_test, input_sh
     input_a = Input(shape=input_shape)
     input_b = Input(shape=input_shape)
 
-    # because we re-use the same instance `base_network`,
-    # the weights of the network
+    # Re-use the same instance base_network so the weights of the network
     # will be shared across the two branches
     processed_a = base_network(input_a)
     processed_b = base_network(input_b)
 
+    # Setup Siamese model with manhattan distance loss function
     malstm_distance = ManDist()([processed_a, processed_b])
     model = Model([input_a, input_b], malstm_distance)
 
     # Train model
+    # Batch size of 500, with 20 epochs and validaiton split of 10%
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     ff = model.fit([x_train['left'], x_train['right']], y_train,
             batch_size=500,
@@ -69,7 +70,7 @@ def generatePlots(name, base_network, x_train, x_test, y_train, y_test, input_sh
     plt.subplot(2, 1, 1)
     plt.plot(ff.history['acc'])
     plt.plot(ff.history['val_acc'])
-    plt.title('LSTM Model Accuracy')
+    plt.title('{0} Model Accuracy'.format(name))
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper left')
@@ -83,24 +84,28 @@ def generatePlots(name, base_network, x_train, x_test, y_train, y_test, input_sh
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Validation'], loc='upper right')
 
+    # Save and clear graph
     plt.tight_layout(h_pad=1.0)
     plt.savefig('./data/history-graph-{0}.png'.format(name))
     plt.clf()
 
+    # Print test Accuracy
     scores = model.evaluate([x_test['left'], x_test['right']], y_test, verbose=0)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-
+# Tokenize and split data
 data = tokenizeData('./data/questions.csv')
 x_train, x_test, y_train, y_test = split(data['question1'], data['question2'], data['label'], 0.85)
 
 input_shape = x_train['right'].shape[1:]
 
+# Create each model
 cnnModel = cnn(input_shape)
 rnnModel = rnn(input_shape)
 lstmModel = lstm(input_shape)
 
-generatePlots('cnnModel', cnnModel, x_train, x_test, y_train, y_test, input_shape)
-generatePlots('rnnModel', rnnModel, x_train, x_test, y_train, y_test, input_shape)
-generatePlots('lstmModel', lstmModel, x_train, x_test, y_train, y_test, input_shape)
+# Train and test each model to generate plots
+generatePlots('Convolutional Neural Network', cnnModel, x_train, x_test, y_train, y_test, input_shape)
+generatePlots('Recurrent Neural Network', rnnModel, x_train, x_test, y_train, y_test, input_shape)
+generatePlots('Long Short-Term Memory Neural Network', lstmModel, x_train, x_test, y_train, y_test, input_shape)
 
